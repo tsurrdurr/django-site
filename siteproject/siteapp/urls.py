a308f3da32473b624ec32bd62cccd3f6334280a2
tsurrdurr <tsurrdurr@users.noreply.github.com>
2017-06-10 21:23:29 +0300

    post template transformed into a page at /siteapp/blog address that has functionality to add sticky notes, edit their text and move them around added static folder, added sticky.css style for notes

--- siteproject/siteapp/urls.py
+++ siteproject/siteapp/urls.py
@@ -4,4 +4,5 @@ from siteapp import views
 urlpatterns = [
     url(r'^time/$', views.today_is, name='todays_time'),
     url(r'^$', views.index, name='siteapp_index'),
+    url(r'^blog/$', views.blog, name='siteapp_index'),
 ]
--- siteproject/siteapp/views.py
+++ siteproject/siteapp/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 import datetime
 
 def index(request):
     return HttpResponse("Hello World in Django")
+
+def blog(request):
+    x = TemplateResponse(request, 'post.html', {})
+    x.render()
+    return HttpResponse(x)
 
 
 def today_is(request):
